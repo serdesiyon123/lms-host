5811c3b4b974d3f21f51c16d566ac11e87d8a51c
Kalkidan Amare <kalkidanamare11a@gmail.com>
2024-03-16 00:20:01 +0300

    the view again

--- manage_books/views.py
+++ manage_books/views.py
@@ -1,1 +1,190 @@
-# gonna be fixed soon.
+from django.shortcuts import render, redirect,get_object_or_404
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.models import User, Group
+from django.contrib import messages
+from django.db.models import Q
+from django.utils import timezone
+from .forms import BooksRegister
+from .models import Books, Borrow,Rating
+from datetime import timedelta
+
+
+
+def homepage(request):
+    if request.method == 'POST':
+        delete = request.POST.get('delete')
+        if delete:
+            book = Books.objects.get(id=delete)
+            book.delete()
+            return redirect('/home')
+
+    '''filtering by Categories'''
+    value = request.GET.get('value')
+    if value and value != 'All':
+        books = Books.objects.filter(genre=value)
+    else:
+        books = Books.objects.all()
+
+    borrowed_books = Borrow.objects.all()
+    return render(request, 'ui/home.html', {'books': books, 'borrowed_books': borrowed_books})
+
+
+# for book rating and review
+def rate_book(request, book_id):
+    if request.method == 'POST':
+        rating_value = int(request.POST.get('rating'))
+        review_text = request.POST.get('review', '')
+        book = get_object_or_404(Books, pk=book_id)
+        Rating.objects.create(book=book, user=request.user, rating=rating_value, review=review_text)
+        return redirect('book_detail', id=book_id)
+    
+    return redirect('/home')
+
+def book_detail(request, id):
+    book = get_object_or_404(Books, pk=id)
+    ratings = Rating.objects.filter(book=book)
+
+    return render(request, '____.html', {'book': book, 'ratings': ratings})
+
+#----------------------------------------------------------------
+
+
+@permission_required('manage_books.add_books', raise_exception=True)
+def update_books(request, id):
+    if request.method == 'POST':
+
+        if id == 0:
+            form = BooksRegister(request.POST, request.FILES)
+        else:
+            book = Books.objects.get(pk=id)
+            form = BooksRegister(request.POST, request.FILES, instance=book)
+        if form.is_valid():
+            form.save()
+        return redirect('/home')
+    elif request.method == 'GET':
+
+        if id == 0:
+            form = BooksRegister()
+            return render(request, 'ui/add_books.html', {'form': form})
+        else:
+            book = Books.objects.get(pk=id)
+            form = BooksRegister(instance=book)
+        return render(request, 'ui/add_books.html', {'form': form})
+
+
+def add_books(request):
+    if request.method == 'POST':
+        form = BooksRegister(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        return redirect('/home')
+    elif request.method == 'GET':
+        form = BooksRegister()
+        return render(request, 'ui/add_books.html', {'form': form})
+
+
+def admin(request):
+    if request.method == 'GET':
+        users = User.objects.filter(Q(groups__name='admin') | Q(groups__name='student'))
+        return render(request, 'ui/admin.html', {'users': users})
+    elif request.method == 'POST':
+        user_id = request.POST.get('user-id')
+        username = request.POST.get('username')
+
+        if user_id:
+            user = User.objects.filter(id=user_id).first()
+            if request.user.is_staff:
+                try:
+                    group = Group.objects.get(name='student')
+                    group.user_set.remove(user)
+                except:
+                    pass
+                try:
+                    group = Group.objects.get(name='admin')
+                    group.user_set.remove(user)
+                except:
+                    pass
+            return redirect('/admin-page')
+        elif username:
+            user = User.objects.filter(username=username).first()
+            if request.user.is_staff:
+                admin_group = Group.objects.get(name='admin')
+                student_group = Group.objects.get(name='student')
+                admin_group.user_set.add(user)
+                student_group.user_set.remove(user)
+            return redirect('/admin-page')
+
+
+def search_result(request):
+    if request.method == 'GET':
+        search = request.GET.get('search')
+        if search is not None:
+            books = Books.objects.filter(
+                Q(name__icontains=search) | Q(author__icontains=search) | Q(genre__icontains=search))
+            return render(request, 'ui/search.html', {'books': books})
+        else:
+            return render(request, 'ui/home.html')
+
+
+def search_users(request):
+    if request.method == 'GET':
+        search_user = request.GET.get('search-user')
+        Users = User.objects.all()
+        users = Users.objects.filter(username__icontains=search_user)
+        return render(request, 'ui/search_users.html', {'users': users})
+    return render(request, 'ui/search_users.html', )
+
+
+@login_required(login_url='/status')
+def status(request):
+    borrowed = Borrow.objects.filter(user=request.user)
+    return render(request, 'ui/status.html', {'borrowed': borrowed})
+
+
+def borrow(request, book_id):
+    if request.method == 'GET':
+        max_limit = 3
+
+        count = Borrow.objects.filter(user=request.user).count()
+
+        if count >= max_limit:
+            messages.error(request,"You have reached max book limit \n please return some book to take more")
+            return redirect('/status')
+        book = Books.objects.get(id=book_id)
+        Borrow.objects.create(
+            id=book_id,
+            book=book,
+            user=request.user,
+            borrow_date=timezone.now(),
+            return_date=timezone.now() + timedelta(weeks=2)
+        )
+        return redirect('description-each', id=book_id)
+
+
+def return_book(request, book_id):
+    if request.method == 'GET':
+       book = Borrow.objects.get(pk=book_id)
+       book.delete()
+       return redirect('status')
+
+
+def description(request,id):
+    if request.method == 'GET':
+        review = ReviewRegister()
+        user_reviews = Rating.objects.all()
+        book = Books.objects.get(pk=id)
+        taken = Borrow.objects.all().values_list('book__id', flat=True)
+        user_borrowed = Borrow.objects.filter(user=request.user,book=book).exists()
+        return render(request, 'ui/description.html', {'book':book , 'taken': taken, 'user_borrowed':user_borrowed,'review': review, 'user_reviews': user_reviews})
+
+    elif request.method == 'POST':
+        review_form= ReviewRegister(request.POST)
+        book = Books.objects.get(pk=id)
+        if review_form.is_valid():
+            review = review_form.save(commit=False)
+            review.user = request.user
+            review.book = book
+            review.review_date = timezone.now()
+            review.id = id
+            review.save()
+        return redirect('description-each', id=id)
