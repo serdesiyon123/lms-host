a6064894e610eb24559c72d944825d9986d8a258
Serdesiyon <serdesiyon.tsegaye@aastustudent.edu.et>
2024-03-17 17:17:20 +0300

    allowed hosts added

--- main/settings.py
+++ main/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4mtz%lfi55fsf0=jdzn0z_9_5t53&3iljmc+ksmgvly&_)p!ck'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lms-host.onrender.com']
 
 
 # Application definition
