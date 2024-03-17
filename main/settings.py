89fc55dcfc355d1d195a7fb1f831b46a1e8572fb
Serdesiyon <serdesiyon.tsegaye@aastustudent.edu.et>
2024-03-17 18:11:06 +0300

    comeon

--- main/settings.py
+++ main/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4mtz%lfi55fsf0=jdzn0z_9_5t53&3iljmc+ksmgvly&_)p!ck'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['lms-host.onrender.com']
 
